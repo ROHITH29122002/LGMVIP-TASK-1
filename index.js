43035b1010e38e81d0282cc1b405b032dd001981
Rohith Challa <rohithsince2002@gmail.com>
2022-07-09 14:21:46 +0530

    some small changes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var jsontasks = localStorage.getItem("tasks");
 let tasks;
 function create_UUID(){
     var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/x|y/g, function(c) {
-        var r = (dt + Math.random()*16)%16 | 0;
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/x|y/g, function(b) {
+        var a = (dt + Math.random()*16)%16 | 0;
         dt = Math.floor(dt/16);
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        return (b=='x' ? a :(a&0x3|0x8)).toString(16);
     });
     return uuid;
 }
